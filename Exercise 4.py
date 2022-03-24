69bcfe6067d40d7ebcd9a1ba3dcca269306bcee5
SW8411 <101498254+SW8411@users.noreply.github.com>
2022-03-24 15:44:19 +0700

    Add files via upload
    Exercise 4

---
+++ Exercise 4.py
@@ -0,0 +1,9 @@
+FE = 75
+GB = 89
+ICS = 67
+CP = 73
+print("---------------Your Score---------------")
+print ("Foundation English :", FE)
+print ("General Business :", GB)
+print ("Introduction to Computer Systems :", ICS)
+print ("Computer Programming :", CP)
