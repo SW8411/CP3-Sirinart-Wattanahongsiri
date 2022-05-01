8aff59e6942ad51cecff4d82ec83a1022202513e
SW8411 <101498254+SW8411@users.noreply.github.com>
2022-05-01 16:41:08 +0700

    Add files via upload

---
+++ Exercise 9_Sirinart_W.py
@@ -0,0 +1,7 @@
+username = input("Username : ")
+password = input("Password : ")
+while username != "admin" or password != "1234" :
+    print("Username/Password is in correct!!")
+    username = input("Username : ")
+    password = input("Password : ")
+print ("You are successfully login !!")
