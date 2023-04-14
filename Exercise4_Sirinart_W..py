7347d857fdbac555c28dc8a59dd858d1c954b007
SW8411 <101498254+SW8411@users.noreply.github.com>
2023-04-14 15:01:12 +0700

    Add files via upload

---
+++ Exercise4_Sirinart_W..py
@@ -0,0 +1,9 @@
+FE= 85
+GB=89.5
+ICS=73.5
+CP=78
+print("---Your score---")
+print("Foundation English:",FE,"/ 100")
+print("General Business:",GB,"/ 100")
+print("Introduction to Computer Systems:",ICS,"/ 100")
+print("Computer Programming:",CP,"/ 100")
